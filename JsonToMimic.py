9500c372ce90fd1eaa2e8e8e344aa6fc30a948a7
SleepingFox <seanloganking@gmail.com>
2019-05-29 02:21:32 -0400

    Create function euler_to_quaternion

---
+++ JsonToMimic.py
@@ -0,0 +1,22 @@
+#Imports
+#===========================================================================
+import numpy as np
+
+# Function declarations
+#===========================================================================\
+
+def euler_to_quaternion(heading, attitude, bank):
+    c1 = np.cos(heading/2)
+    s1 = np.sin(heading/2)
+    c2 = np.cos(attitude/2)
+    s2 = np.sin(attitude/2)
+    c3 = np.cos(bank/2)
+    s3 = np.sin(bank/2)
+    c1c2 = c1 * c2
+    s1s2 = s1 * s2
+    w = c1c2*c3 - s1s2*s3
+    x = c1c2*s3 + s1s2*c3
+    y = s1*c2*c3 + c1*s2*s3
+    z = c1*s2*c3 - s1*c2*s3
+
+    return [w,x,y,z]
