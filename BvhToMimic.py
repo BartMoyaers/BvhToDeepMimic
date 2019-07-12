9a3ce7f368930c0d57cbc86bf30647414a2cac4c
SleepingFox <seanloganking@gmail.com>
2019-07-12 04:11:38 -0400

    gets keyframe's time and root position

--- BvhToMimic.py
+++ BvhToMimic.py
@@ -32,8 +32,11 @@ def euler_to_quaternion(heading, attitude, bank):
     x = c1c2*s3 + s1s2*c3
     y = s1*c2*c3 + c1*s2*s3
     z = c1*s2*c3 - s1*c2*s3
-
     return [w, x, y, z]
+
+
+def bvhBoneName(mimicBone):
+    return humanoidRig[f"{mimicBone}"]
 
 
 # Initialization
@@ -41,13 +44,17 @@ def euler_to_quaternion(heading, attitude, bank):
 
 removeAllFilesInDirectory("./OutputMimic/")
 
+# get json of humanoidRig
+with open(f"./Rigs/humanoidRig.json") as json_data:
+    humanoidRig = json.load(json_data)
+
 # Sets up list of bones used by DeepMimic humanoid
 # Order is important
 deepMimicHumanoidJoints = ["seconds", "hip", "hip", "chest", "neck", "right hip", "right knee", "right ankle",
                            "right shoulder", "right elbow", "left hip", "left knee", "left ankle", "left shoulder", "left elbow"]
 dimensions = [1, 3, 4, 4, 4, 4, 1, 4, 4, 1, 4, 1, 4, 4, 1]
 
-# Locks root rotation for dev testing
+# Locks root position and rotation for dev testing
 posLocked = True
 
 # sets onlyfiles to a list of files founds in the "mypath" directory
@@ -65,8 +72,28 @@ for j in range(0, len(onlyfiles)):
         mocap = Bvh(f.read())
 
         # For every keyFrame
-        keyFrame = []
         for i in range(0, mocap.nframes):
-            print(i)
+            keyFrame = []
 
-            # print(mocap.frame_joint_channel(22, 'Head', 'Xrotation'))
+            # for all DeepMimicHumanoid Joints
+            for p in range(0, len(deepMimicHumanoidJoints)):
+                # Append Time
+                if p == 0:
+                    keyFrame.append(mocap.frame_time)
+
+                # Append root position
+                elif p == 1:
+                    # X
+                    keyFrame.append(mocap.frame_joint_channel(
+                        i, bvhBoneName(deepMimicHumanoidJoints[p]), 'Xposition'))
+                    # Y
+                    keyFrame.append(mocap.frame_joint_channel(
+                        i, bvhBoneName(deepMimicHumanoidJoints[p]), 'Yposition'))
+                    # Z
+                    keyFrame.append(mocap.frame_joint_channel(
+                        i, bvhBoneName(deepMimicHumanoidJoints[p]), 'Zposition'))
+                # elif dimensions[p] == 1:
+                #     keyFrame.append()
+                    # print(bvhBoneName(deepMimicHumanoidJoints[p]))
+
+            print(i, keyFrame)
