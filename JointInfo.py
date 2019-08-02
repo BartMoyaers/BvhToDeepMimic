bb6ff01460192621cd19676121a84f4d566354e4
Bart Moyaers <bart.moyaers@kuleuven.be>
2019-08-02 15:54:11 +0200

    remove redundant method

--- JointInfo.py
+++ JointInfo.py
@@ -16,34 +16,6 @@ class JointInfo:
         self.dimensions = dimensions
         self.zeroRotVector = zeroRotVector
 
-    def calculateOffsetQuat(self) -> Quaternion:
-        v1 = np.array(self.zeroRotVector)
-        v2 = np.array(self.childOffset)
-
-        # Normalize vectors
-        norm1 = np.linalg.norm(v1)
-        norm2 = np.linalg.norm(v2)
-        if norm1 > 0:
-            v1 = v1 / norm1
-        if norm2 > 0:
-            v2 = v2 / norm2
-
-        # Calculate perpendicular vector
-        perpvec = np.cross(v1, v2)
-        perpnorm = np.linalg.norm(perpvec)
-        if perpnorm > 0:
-            perpvec = perpvec / perpnorm
-            angle = math.acos(np.dot(v1, v2))
-        else:
-            perpvec = np.array([1, 0, 0])
-            angle = 0
-
-        # Calculate quaternion from angle axis form.
-        result = Quaternion(axis=perpvec, radians=angle)
-
-        # Note that this quaternion is defined in the frame of the BVH file.
-        return self.quatBvhToDM(result)
-
     @staticmethod
     def quatBvhToDM(quaternion: Quaternion) -> Quaternion:
         # transform x -> z and z -> -x
