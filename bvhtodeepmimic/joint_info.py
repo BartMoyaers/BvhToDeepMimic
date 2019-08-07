64597e572a9d0f7c2e9ee83f6af68decd37c2553
Bart Moyaers <bart.moyaers@kuleuven.be>
2019-08-07 11:36:56 +0200

    remove redundant methods

--- bvhtodeepmimic/joint_info.py
+++ bvhtodeepmimic/joint_info.py
@@ -1,11 +1,7 @@
-import numpy as np
-import math
 from typing import List
-from pyquaternion import Quaternion
 
 class JointInfo:
-    """ Contains information on a certain joint in the DeepMimic human model as 
-    well as some functions in order to convert joint data from BVH to DeepMimic.
+    """ Contains information on a certain joint in the DeepMimic human model.
     """
 
     def __init__(self, deepMimicName: str, bvhName: str, 
@@ -15,22 +11,3 @@ class JointInfo:
         # self.bvhChildName = bvhChildName
         self.dimensions = dimensions
         self.zeroRotVector = zeroRotVector
-
-    @staticmethod
-    def quatBvhToDM(quaternion: Quaternion) -> Quaternion:
-        # transform x -> z and z -> -x
-        return Quaternion(
-            quaternion[0],
-            quaternion[3],
-            quaternion[2],
-            -quaternion[1]
-        )
-
-    @staticmethod
-    def posBvhToDM(translation: List[float]) -> List[float]:
-        # transform x -> z and z -> -x
-        return [
-            translation[2],
-            translation[1],
-            -translation[0]
-        ]
