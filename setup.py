3cc989aafbc535f850ca6b0e48286245ea3aa22d
Bart Moyaers <bart.moyaers@kuleuven.be>
2019-08-07 13:49:18 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bvhtodeepmimic",
+    version="0.0.1",
+    author="Bart Moyaers",
+    author_email="bart.moyaers@gmail.com",
+    description="Convert .bvh files (Biovision Hierarchy) to DeepMimic format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BartMoyaers/BvhToDeepMimic",
+    packages=["bvhtodeepmimic"],
+    py_modules=["bvhtomimic"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    requires=["pyquaternion", "numpy", "bvh", "tqdm"]
+)
